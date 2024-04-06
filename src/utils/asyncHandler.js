b4adcf0401bd3176bc8abdcd3bc8055ca6cd8a6c
Kartik-ui <imkartik1311@gmail.com>
2024-04-07 00:12:01 +0530

    user controller completed and subscription model added

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,11 @@
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error)
+      // next(error)
+      res.status(error.statusCode || 500).json({
+        success: false,
+        message: error.message || "Internal server error",
+      })
     );
   };
 };
